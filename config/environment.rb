dfad68a106ccefb3abbf5f3ed78ed8cac1996989
RealSilo <szilard.magyar@gmail.com>
2015-08-29 22:02:05 +0200

    sendgrid+devise

--- config/environment.rb
+++ config/environment.rb
@@ -4,7 +4,7 @@ require File.expand_path('../application', __FILE__)
 # Initialize the Rails application.
 Rails.application.initialize!
 
-ActionMailer::Base.delivery_method = :smtp
+
 ActionMailer::Base.smtp_settings = {
   :address        => 'smtp.sendgrid.net',
   :port           => '587',
@@ -14,3 +14,6 @@ ActionMailer::Base.smtp_settings = {
   :domain         => 'heroku.com',
   :enable_starttls_auto => true
 }
+
+ActionMailer::Base.delivery_method = :smtp
+ActionMailer::Base.default charset: "utf-8"
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  #setting production email
+  config.action_mailer.default_url_options = { host: 'appfaskyn.herokuapp.com' }
 end
