ae753ad857f7839dccfbea22f95c9ac409ff6fc8
RealSilo <szilard.magyar@gmail.com>
2015-09-29 14:28:12 +0200

    sidekiqworking

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -50,14 +50,15 @@ class TasksController < ApplicationController
     if @task.save
       #@assigner = @task.assigner
       #h = JSON.generate({ 'name' => @assigner.profile.name })
-      h = JSON.generate( {'task_assigner_first_name' => @current_user.profile.first_name,
-                          'task_assigner_last_name' => @current_user.profile.last_name,
-                          'task_executor_first_name' => @task.executor.profile.first_name,
-                          'task_executor_email' => @task.executor.email,
-                          'task_executor_id' => @task.executor_id,
-                          'task_id' => @task.id
-                           } )
-      TaskcreatorWorker.perform_async(h, 5)
+      #h = JSON.generate( {'task_assigner_first_name' => @task.assigner.profile.first_name,
+                          #'task_assigner_last_name' => @task.assigner.profile.last_name,
+                          #'task_executor_first_name' => @task.executor.profile.first_name,
+                          #'task_executor_email' => @task.executor.email,
+                          #'task_executor_id' => @task.executor_id,
+                          #'task_id' => @task.id
+                          #} )
+      #TaskcreatorWorker.perform_async(h, 5)
+      TaskcreatorWorker.perform_async(@task.id, @user.id, 5)
       #TaskMailer.task_created(current_user, @task).deliver_later
       flash[:success] = "Task saved!"
       redirect_to user_tasks_path(current_user)
--- app/mailers/task_mailer.rb
+++ app/mailers/task_mailer.rb
@@ -1,16 +1,20 @@
 class TaskMailer < ActionMailer::Base
 
-  def task_created(task_assigner_first_name, task_assigner_last_name, task_executor_first_name, task_executor_email, task_executor_id, task_id) 
-    @task.assigner.profile.first_name = task_assigner_first_name
-    @task.assigner.profile.last_name = task_assigner_last_name
-    @task.executor.profile.first_name = task_executor_first_name
-    @task.executor.email = task_executor_email
-    @task.executor_id = task_executor_id
-    @task.id = task_id
+  #def task_created(task_assigner_first_name, task_assigner_last_name, task_executor_first_name, task_executor_email, task_executor_id, task_id) 
+    #@task.assigner.profile.first_name = task_assigner_first_name
+    #@task.assigner.profile.last_name = task_assigner_last_name
+    #@task.executor.profile.first_name = task_executor_first_name
+    #@task.executor.email = task_executor_email
+    #@task.executor_id = task_executor_id
+    #@task.id = task_id
+
+  def task_created(task, user)
+    @task = task
+    @current_user = user
 
     mail(from: 'faskyn@gmail.com',
-         to: "#{@task.executor.email}",
-         subject: "[Faskyn] New task/favor from #{@task.assigner.profile.first_name} #{@task.assigner.profile.last_name}"
+         to: "#{task.executor.email}",
+         subject: "[Faskyn] New task/favor from #{task.assigner.profile.first_name} #{task.assigner.profile.last_name}"
          )
   end
 end
