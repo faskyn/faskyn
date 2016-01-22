84b745225272d513e8b1791699c693adedc926c8
RealSilo <szilard.magyar@gmail.com>
2016-01-22 15:22:56 -0800

    CORS for cdn served fonts

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,6 +59,7 @@ Rails.application.configure do
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   config.action_controller.asset_host = 'd24x4pqz7khqm2.cloudfront.net'
+  config.font_assets.origin = 'appfaskyn.herokuapp.com'
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
