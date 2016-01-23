924006cd9f9232f99246c57345c038eaf1eee7e7
RealSilo <szilard.magyar@gmail.com>
2016-01-22 16:42:44 -0800

    finishing CORS setup

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,7 +59,7 @@ Rails.application.configure do
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   config.action_controller.asset_host = 'd24x4pqz7khqm2.cloudfront.net'
-  config.font_assets.origin = 'http://appfaskyn.herokuapp.com'
+  config.font_assets.origin = '*'
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
