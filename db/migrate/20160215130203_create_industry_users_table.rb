1f9124407490fbbbcb78b9825204f04e909bc83b
RealSilo <szilard.magyar@gmail.com>
2016-02-20 21:08:10 +0100

    deleting unnecessary migration files

--- db/migrate/20160215130203_create_industry_users_table.rb
+++
@@ -1,8 +0,0 @@
-class CreateIndustryUsersTable < ActiveRecord::Migration
-  def change
-    create_table :industry_users_tables do |t|
-      t.references :user, index: true
-      t.references :industry, index: true
-    end
-  end
-end
--- db/migrate/20160215130718_drop_industry_user_table.rb
+++
@@ -1,5 +0,0 @@
-class DropIndustryUserTable < ActiveRecord::Migration
-  def change
-    drop_table :industry_users_tables
-  end
-end
