12e9f6747d9a799b8685b442b1f84c84bb3d670c
RealSilo <szilard.magyar@gmail.com>
2016-03-02 18:52:40 +0100

    flash message design

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -2,11 +2,19 @@ $(document).on("page:change", function() {
   $('[data-toggle="tooltip"]').tooltip();
 
   //flash messages fade out after 3.5 sec
-  setTimeout(function(){
-		$('.alert').fadeOut(2500, function(){
-			$('.flash-container').remove();
-		})
-	}, 3500);
+ //  setTimeout(function(){
+	// 	$('.alert').fadeOut(2500, function(){
+	// 		$('.flash-container').remove();
+	// 	})
+	// }, 3500);
+
+var alert = $('.alert.flash-alert');
+  if (alert.length > 0) {
+    alert.show().animate({height: alert.outerHeight()}, 200);
+    window.setTimeout(function() {
+      alert.slideUp();
+    }, 3000);
+  }
 });
 
 
