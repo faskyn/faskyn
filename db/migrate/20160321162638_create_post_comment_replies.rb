b8db959a468e6216b242f4ff6211971d46a705ef
RealSilo <szilard.magyar@gmail.com>
2016-03-22 18:51:53 +0100

    migration for replies

--- db/migrate/20160321162638_create_post_comment_replies.rb
+++ db/migrate/20160321162638_create_post_comment_replies.rb
@@ -1,6 +1,5 @@
 class CreatePostCommentReplies < ActiveRecord::Migration
   def change
-    drop_table :post_comment_replies
     create_table :post_comment_replies do |t|
       t.references :user, index: true
       t.references :post_comment, index: true
