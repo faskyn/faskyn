c9a31d1dbbb2e0748eeed550a537e15b7064765d
RealSilo <szilard.magyar@gmail.com>
2016-03-29 21:45:41 +0200

    adding caching to static asset CDN

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,6 +76,7 @@ Rails.application.configure do
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   config.action_controller.asset_host = ENV["CLOUDFRONT_URL"]
   config.font_assets.origin = '*'
+  config.static_cache_control = "public, max-age=31536000"
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
